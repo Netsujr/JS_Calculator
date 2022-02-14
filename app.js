9ffa1d430b333ec8c29a79cd75018e1c2c7d54ff
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-14 19:25:33 +0900

    assigining JS buttons

---
+++ app.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+
+const previousOperandTextElement = document.querySelector('[data-previous]');
+const currentOperandTextElement = document.querySelector('[data-current]');
